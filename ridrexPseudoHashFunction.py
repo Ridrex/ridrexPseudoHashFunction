62828c53c477746c35778aba19147f586c2c0ebd
Ridrex <96786670+Ridrex@users.noreply.github.com>
2021-12-29 19:18:44 +0100

    Updated the main function
    Massive improvement to speed, algorithm change (this change will make this version incompatible with the first version!), as well as more concise code. Now takes ASCII as input and gives back a string of integers as output.

--- ridrexPseudoHashFunction.py
+++ ridrexPseudoHashFunction.py
@@ -1,78 +1,128 @@
 
-# Ridrex's custom (probably crappy) hash function, enjoy!
+# Hash function. Input must be a string of ASCII characters. Output is a string of ints.
+# All other inputs are integers.
+# Outputs the hash and number of blocks
+def ridrexPseudoHashFunction(input, desiredOutputLength, blockSize, blockExponentLoop, wholeHashModLoop, exponentiator):
 
-# Turn a string or int into a list of integers.
-def makeIntList(input):
-    return [int(d) for d in str(input)]
+    # THIS SECTION PREPARES INPUT FOR HASH FUNCTION
 
+    # Minimum number of blocks for blockwise mod addition
+    # This is hardcoded to ensure blockwise addition functions
+    minBlocks = 2
 
-# Hash function. Input must be an integer. Output is a string.
-# desiredOutputLength is how many characters you want the hash to be.
-# difficulty can be any integer 1 or higher, more means better security but longer wait time.
-def ridrexPseudoHashFunction(input, desiredOutputLength, difficulty):
+    # Minimum ratio of length required to ensure an output of sufficient length
+    # Hardcoded to ensure functionality
+    lengthToOutputRatio = 4
 
-    # Turn input int into list
-    input = makeIntList(input)
+    # Blocks dictionary
+    blocks = {}
 
-    initialLen = len(input)
+    # Make sure the input is a string
+    input = str(input)
 
-    # Modular addition, add five to first int in list
-    firstInt = int(input[0])
-    if firstInt // 5 >= 1:
-        firstInt -= 5
-    else:
-        firstInt += 5
-    input[0] = firstInt
-
-    # Blow up the initial integer if it's too small
-    tempStr = ''
-    while len(input) < desiredOutputLength:
-        for i in range(initialLen-1):
-            eye1 = int(input[i+1])
-            eye = int(input[i])
-            workNum = eye1 ** 7 + eye
-            workNum = str(workNum)
-            tempStr = tempStr + workNum
-            #print(tempStr)
-        input = makeIntList(tempStr)
-
-    # Shrink it down if it got too big
-    tempStr = ''
-    if len(input) > desiredOutputLength:
-        for i in input:
-            tempStr = tempStr + str(i)
-        tempStr = tempStr[0:desiredOutputLength]
-        input = makeIntList(tempStr)
-
-
-    # Repeat modular addition back and forth steps 21 times
-    for i in range(difficulty):
-
-        # Use twenty-first power of previous number for modular addition to current
-        for i in range(len(input)-1):
-            eye1 = int(input[i+1])
-            eye = int(input[i])
-            workNum = eye1 ** 21 + eye
-            workNum = str(workNum)
-            workNum = workNum[-1]
-            workNum = int(workNum)
-            input[i+1] = workNum
-
-        # Go back through the list with modular addition
-        for i in range(len(input)):
-            eye1 = int(input[-i-1])
-            eye = int(input[-i])
-            workNum = eye1 ** 21 + eye
-            workNum = str(workNum)
-            workNum = workNum[-1]
-            workNum = int(workNum)
-            input[-i-1] = workNum
-
-    # Turn list back into integer
-    tempInt = ''
+    # Turn all ascii characters into an integer
+    tempString = ''
     for i in input:
-        tempInt = tempInt + str(i)
-    tempInt = str(tempInt)
-    input = tempInt
+        tempString = tempString + str(ord(i))
+    input = tempString
+
+    # Calculate how many blocks there are
+    blockCount = len(input) // blockSize
+
+    # Make sure that minimum blocks requirement is met
+    if len(input) - (minBlocks * blockSize) < 0:
+        padding = (minBlocks * blockSize) - len(input)
+        for i in range(padding):
+            input = input + '0'
+        # Recalculate how many blocks there are
+        blockCount = len(input) // blockSize
+
+    # Make sure there is enough data to satisfy the length to output ratio
+    if len(input) - (desiredOutputLength * lengthToOutputRatio) < 0:
+        padding = (desiredOutputLength * lengthToOutputRatio) - len(input)
+        for i in range(padding):
+            input = input + '0'
+        # Recalculate how many blocks there are
+        blockCount = len(input) // blockSize
+
+    # Pad data to fill in the last block if applicable
+    if len(input) % (blockCount * blockSize) > 0:
+        padding = ((blockCount + 1) * blockSize) - len(input)
+        for i in range(padding):
+            input = input + '0'
+        # Recalculate how many blocks there are
+        blockCount = len(input) // blockSize
+
+    # Add all of the data into the dictionary blockwise
+    for i in range(blockCount):
+        currentBlockData = str(input[i*blockSize:(i+1)*blockSize])
+        currentBlockName = str(i)
+        blocks[currentBlockName] = currentBlockData
+
+    # ACTUAL HASH FUNCTION STARTS HERE
+
+    # Perform exponentiation and truncation on individual blocks
+    workNum = ''
+    for i in range(blockCount):
+        workNum = blocks[str(i)]
+        for y in range(blockExponentLoop):
+            workNum = int(workNum) ** exponentiator
+            workNum = str(workNum)
+            workNum = workNum[0:blockSize]
+            blocks[str(i)] = workNum
+        workNum = ''
+
+    # Repeat modular addition back and forth steps
+    for i in range(wholeHashModLoop):
+
+        # Modular addition forward blockwise
+        workNum = ''
+        workNum2 = ''
+        sum = 0
+        for i in range(blockCount-1):
+            workNum = blocks[str(i)]
+            workNum2 = blocks[str(i+1)]
+            workNum = int(workNum)
+            workNum2 = int(workNum2)
+            sum = workNum + workNum2
+            sum = str(sum)
+            sum = sum[0 - (blockSize):]
+            # Pad if result is not long enough
+            if len(sum) < blockSize:
+                for i in range(blockSize - len(sum)):
+                    sum = sum + '0'
+            blocks[str(i+1)] = sum
+            workNum = ''
+            workNum2 = ''
+
+        # Go back through the list with modular addition blockwise
+        workNum = ''
+        workNum2 = ''
+        sum = 0
+        for i in range(blockCount-1):
+            workNum = blocks[str((blockCount-1)-i-1)]
+            workNum2 = blocks[str((blockCount-1)-i)]
+            workNum = int(workNum)
+            workNum2 = int(workNum2)
+            sum = workNum + workNum2
+            sum = str(sum)
+            sum = sum[0 - (blockSize):]
+            # Pad if result is not long enough
+            if len(sum) < blockSize:
+                for i in range(blockSize - len(sum)):
+                    sum = sum + '0'
+            blocks[str((blockCount-1)-i-1)] = sum
+            workNum = ''
+            workNum2 = ''
+
+    # Reconstruct the string from dictionary
+    tempString = ''
+    for i in blocks:
+        tempString = tempString + blocks[i]
+    input = tempString
+
+    # Truncate it if it's too long
+    if len(input) > desiredOutputLength:
+        input = input[0:desiredOutputLength]
 
     return(input)
